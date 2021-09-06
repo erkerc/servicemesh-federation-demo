a6e8b35d7caf6d95eff8a1d61a15a32302eaa507
erkerc <erkan.ercan@gmail.com>
2021-09-06 12:58:43 +0300

    updated configs

--- red-load.sh
+++ red-load.sh
@@ -1,5 +1,5 @@
 #export GATEWAY_URL=http://bookinfo-bookinfo-gateway-lh2rt-istio-tutorial.apps.cluster-ist-6c27.ist-6c27.example.opentlc.com
-export APP_URL=$(oc get route -n red-istio-system |grep istio-ingressgateway-red| awk '{print $2}')
+export APP_URL=$(oc get route -n red-mesh |grep istio-ingressgateway-red| awk '{print $2}')
 while true 
 do
 curl -k -o /dev/null -s -w "%{http_code}\n" $APP_URL/productpage
