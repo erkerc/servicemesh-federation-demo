bf78c69ee8497a8e091eeeb64bb4c42cfb79eabe
erkerc <erkan.ercan@gmail.com>
2021-09-10 13:05:33 +0300

    new configs

--- blue-load.sh
+++ blue-load.sh
@@ -1,4 +1,4 @@
-export APP_URL=$(oc get route -n blue-istio-system |grep istio-ingressgateway-blue| awk '{print $2}')
+export APP_URL=$(oc get route -n blue-mesh |grep istio-ingressgateway-blue| awk '{print $2}')
 while true 
 do
 curl -k -o /dev/null -s -w "%{http_code}\n" $APP_URL/productpage
