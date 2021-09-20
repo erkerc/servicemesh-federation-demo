f9c9a431cffdd12a2e78c8cf2c3470249a124096
erkerc <erkan.ercan@gmail.com>
2021-09-20 23:04:04 +0300

    correct helper

--- smfederation-helper.sh
+++ smfederation-helper.sh
@@ -1,8 +1,8 @@
 # Install?  Retrieve?  common root certificate authority (CA) in Red Mesh  
-export REDMESH_CERT=$(oc get configmap -n red-mesh istio-ca-root-cert -o jsonpath='{.data.root-cert\.pem}' | sed ':a;N;$!ba;s/\n/\\\n      /g')
+export REDMESH_CERT=$(oc get configmap -n red-istio-system istio-ca-root-cert -o jsonpath='{.data.root-cert\.pem}' | sed ':a;N;$!ba;s/\n/\\\n      /g')
 echo $REDMESH_CERT > redmesh_cert
 # Install?  Retrieve?  common root certificate authority (CA) in Blue Mesh  
-export BLUEMESH_CERT=$(oc get configmap -n blue-mesh istio-ca-root-cert -o jsonpath='{.data.root-cert\.pem}' | sed ':a;N;$!ba;s/\n/\\\n      /g')
+export BLUEMESH_CERT=$(oc get configmap -n blue-istio-system istio-ca-root-cert -o jsonpath='{.data.root-cert\.pem}' | sed ':a;N;$!ba;s/\n/\\\n      /g')
 echo $BLUEMESH_CERT > bluemesh_cert
 
 # create Blue Mesh Cert in red-istio-system
