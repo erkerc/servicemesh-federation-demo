76c719e27edd7a2c78fd6c8ccd0302db2e55d7be
erkerc <38360349+erkerc@users.noreply.github.com>
2021-10-26 13:56:38 +0300

    Update bookinfo-deploy.sh

--- bookinfo-deploy.sh
+++ bookinfo-deploy.sh
@@ -1,3 +1,12 @@
 oc create -n bookinfo -f  bookinfo/bookinfo-deployment.yaml
 oc create -n bookinfo -f  bookinfo/bookinfo-gateway.yaml
 oc create -n bookinfo -f  bookinfo/bookinfo-destinationrules-all-mtls.yaml
+oc label deployment/productpage-v1 app.openshift.io/runtime=python
+oc label deployment/reviews-v1 app.openshift.io/runtime=java
+oc label deployment/reviews-v2 app.openshift.io/runtime=java
+oc label deployment/reviews-v3 app.openshift.io/runtime=java
+oc label deployment/ratings-v1 app.openshift.io/runtime=nodejs
+oc label deployment/details-v1 app.openshift.io/runtime=ruby 
+oc annotate deployment/productpage-v1 app.openshift.io/connects-to=reviews-v1,reviews-v2,reviews-v3,details-v1 --overwrite
+oc annotate deployment/reviews-v2 app.openshift.io/connects-to=ratings-v1 --overwrite
+
