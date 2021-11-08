f63f4ed252ae0797ff9bc810668e619d360e4a09
erkerc <erkan.ercan@gmail.com>
2021-11-08 13:00:51 +0300

    updated bookinfo-load

--- bookinfo-load.sh
+++ bookinfo-load.sh
@@ -1,6 +1,6 @@
-export APP_URL=$(oc get route -n istio-system |grep istio-ingressgateway | awk '{print $2}')
+export APP_URL=$(oc get route -n istio-system |grep bookinfo-gw | awk '{print $2}')
 while true 
 do
 curl -k -o /dev/null -s -w "%{http_code}\n" $APP_URL/productpage
-sleep 2 
+sleep 1 
 done
